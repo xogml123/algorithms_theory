ce09ce1db833a2f7afeabeb7d2b28cf315dbead2
xogml123 <xogml951@gmail.com>
2021-05-13 17:35:51 +0900

    merge_sort perfect

--- merge_sort/merge_sort.c
+++ merge_sort/merge_sort.c
@@ -6,59 +6,76 @@ int	*input_array(int n)
 	int	i;
 	int *arr;
 
-	
 	arr = malloc(sizeof(int) * n);
 	i = 0;
 	printf("input array with space\n");
 	while (i < n)
 	{
-		scanf(" %d", &arr[i]);
+		scanf("%d", &arr[i]);
 		i++;
 	}
 	return (arr);
 }
 
-void	merge(int *result, int *arr, int s, int f)
+void	merge(int *arr, int s, int f)
 {
-	int	mid;
+	int	mid = (f + s) / 2;
+	int	left[mid + 1];
+	int	right[f - mid];
 	int	i;
 	int	j;
-	int	min;
-	int	r_i;
+	int	arr_i;
 
-	min = -10000000;
-	mid = (s + f)/2;
-	i = s;
-	j = mid + 1;
-	r_i = s;
-	while(i == mid || j == f)
+	i = 0;
+	j = s;
+	while (i < mid + 1)
 	{
-		if (arr[i] <= arr[j])
-		{
-			result[r_i] = arr[i];
-			i++;
-			r_i;
-		}
-		else
-		{
-			result[r_i] = arr[j];
-			j++;
-			r_i;
-		}
+		left[i] = arr[j];
+		i++;
+		j++;
 	}
-	if (i == mid)
+	i = 0;
+	j = mid + 1;
+	while (i < f - mid)
+	{
+		right[i] = arr[j];
+		i++;
+		j++;
+	}
+	i = 0;
+	j = 0;
+	arr_i = s;
+	while (1)
+	{
+		if (i == mid + 1 || j == f - mid)
+			break;
+		if (left[i] <= right[j])
+			arr[arr_i++] = left[i++];
+		if (left[i] > right[j])
+			arr[arr_i++] = right[j++];
+	}
+	if (i == mid + 1)
+	{
+		while (j < f - mid)
+			arr[arr_i++] = right[j++];
+	}
+	else if (j == f - mid)
+	{
+		while (i < mid + 1)
+			arr[arr_i++] = left[i++];
+	}
 }
 
-void	merge_sort(int *result, int *arr, int s, int f)
+void	merge_sort(int *arr, int s, int f)
 {
 	int	mid;
 
-	if (n > 1)
+	if (f - s + 1 > 1)
 	{
 		mid = (s + f)/2;
-		merge_sort(result, arr, s, mid);
-		merge_sort(result, arr, mid + 1, f);
-		merge(result, arr, s, f);
+		merge_sort(arr, s, mid);
+		merge_sort(arr, mid + 1, f);
+		merge(arr, s, f);
 	}
 }
 
@@ -67,16 +84,13 @@ int main(int argc, char **argv)
 	int	*arr;
 	int	target;
 	int	n;
-	int	*result;
 
 	printf("input array size : ");
 	scanf("%d", &n);
 	arr = input_array(n);
-	result = malloc(sizeof(int) * n);
-	merge_sort(result, arr, 0, n - 1);
+	merge_sort(arr, 0, n - 1);
 	for (int i = 0; i < n; i++)
-		printf("%d ", result[i]);
+		printf("%d ", arr[i]);
 	printf("\n");
-	free(result);
 	free(arr);
 }
